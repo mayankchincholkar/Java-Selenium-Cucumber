077fbec9daff4594d6b971bcd8ce9e1d4126a51f
mayankchincholkar <mayankchincholkar@gmail.com>
2018-12-04 21:20:06 +1100

    Navigate to URL

--- Java-Cucumber-Selenium/resources/features/Test.feature
+++ Java-Cucumber-Selenium/resources/features/Test.feature
@@ -1,5 +1,8 @@
 Feature: Login Feature
   Verify if user is able to Login in to the site
 
+  Background:
+    Given user opens url "https://www.anz.com.au/personal/home-loans/calculators-tools/much-borrow/"
+
   Scenario: Login as a authenticated user
     Given user is  on homepage
--- Java-Cucumber-Selenium/src/test/java/com/cucumber/steps/E2ESteps.java
+++ Java-Cucumber-Selenium/src/test/java/com/cucumber/steps/E2ESteps.java
@@ -1,15 +1,32 @@
 package com.cucumber.steps;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.logging.Logger;
 
 public class E2ESteps {
     private static Logger logger = Logger.getLogger(E2ESteps.class.getName());
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        logger.info("Inside before");
+        System.setProperty("webdriver.chrome.driver","./resources/drivers/chrome-driver/chromedriver");
+        driver= new ChromeDriver();
+    }
 
     @Given("^user is  on homepage$")
     public void user_is_on_homepage() throws Throwable {
         logger.info("Inside steps");
     }
+
+    @Given("^user opens url \"([^\"]*)\"$")
+    public void user_opens_url(String url) throws Throwable {
+        driver.get(url);
+    }
+
 }
